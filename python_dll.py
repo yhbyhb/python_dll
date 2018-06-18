2369082ad5633497004137bbc284b0fffdb17bec
HanByul Yang <hanbyul.yang@gmail.com>
2018-06-18 15:19:35 +0900

    added vector add python dll

--- python_dll.py
+++ python_dll.py
@@ -1,14 +1,30 @@
 import ctypes
+import numpy as np
 
 hllDll = ctypes.WinDLL ("python_dll.dll")
 
-
 test_print_console = hllDll['test_print_console'] # need the extern!!
 test_print_console.argtypes = ()
-test_print_console.restype = None
-test_print_console()
+test_print_console.restype = ctypes.c_int
+print(test_print_console())
 
-mydll = ctypes.cdll.LoadLibrary("python_dll.dll")
-print(mydll)
+test_vector_add = hllDll['test_vector_add'] # need the extern!!
+test_vector_add.argtypes = (ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p, ctypes.c_int,)
+test_vector_add.restype = ctypes.c_int
 
-mydll.test_print_console()
+length = 10
+a = np.ones((length, 1), dtype=np.float32)
+b = np.ones((length, 1), dtype=np.float32) * 2
+c = np.zeros((length, 1), dtype=np.float32)
+
+print(
+	test_vector_add(
+		ctypes.c_void_p(c.ctypes.data), 
+		ctypes.c_void_p(a.ctypes.data), 
+		ctypes.c_void_p(b.ctypes.data),
+		ctypes.c_int(length))
+	)
+
+print(a)
+print(b)
+print(c)
--- python_dll/python_dll.h
+++ python_dll/python_dll.h
@@ -6,6 +6,8 @@ extern "C" {
 
     __declspec(dllexport) int test_print_console();
 
+    __declspec(dllexport) int test_vector_add(float* c, float* a, float* b, int length);
+
 #ifdef __cplusplus
 }
 #endif
