93cea97886d721d6c3d260bab11295f84f3bc55b
HanByul Yang <hanbyul.yang@gmail.com>
2018-08-16 16:00:21 +0900

    another way to using dll in python. no need to args and return description

--- python_dll.py
+++ python_dll.py
@@ -1,16 +1,16 @@
 import ctypes
 import numpy as np
 
-hllDll = ctypes.WinDLL ("python_dll.dll")
+hllDll = ctypes.windll.LoadLibrary("python_dll.dll")
 
-test_print_console = hllDll['test_print_console'] # need the extern!!
-test_print_console.argtypes = ()
-test_print_console.restype = ctypes.c_int
+test_print_console = hllDll.test_print_console # need the extern!!
+# test_print_console.argtypes = ()
+# test_print_console.restype = ctypes.c_int
 print(test_print_console())
 
-test_vector_add = hllDll['test_vector_add'] # need the extern!!
-test_vector_add.argtypes = (ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p, ctypes.c_int,)
-test_vector_add.restype = ctypes.c_int
+test_vector_add = hllDll.test_vector_add # need the extern!!
+# test_vector_add.argtypes = (ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p, ctypes.c_int,)
+# test_vector_add.restype = ctypes.c_int
 
 length = 10
 a = np.ones((length, 1), dtype=np.float32)
